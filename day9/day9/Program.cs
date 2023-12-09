7923522c1723f3f3f92c64cc1d559dccc7193f2e
alex <alex.ioan.dragomir@gmail.com>
2023-12-09 20:17:23 +0200

    day9 functional

--- day9/day9/Program.cs
+++ day9/day9/Program.cs
@@ -1,23 +1,18 @@
 ﻿var report = File.ReadAllLines("in.txt").Select(l => l.Split(" ").Select(n => int.Parse(n)).ToList()).ToList();
 
-foreach(var history in report)
+var added = report.Select(history =>
 {
   List<List<int>> processedHistory = new List<List<int>>(){ history };
-  
-  while(processedHistory.Last().Any(c => c != 0))
-  {
-    List<int> newValues = new List<int>();
-    for(int i = 0; i < processedHistory.Last().Count-1; i++)
-      newValues.Add(processedHistory.Last()[i+1] - processedHistory.Last()[i]);
-    processedHistory.Add(newValues);
-  }
 
-  processedHistory.Last().Insert(0,0);
+  while (processedHistory.Last().Any(c => c != 0))
+    processedHistory.Add(processedHistory.Last().Skip(1).Zip(processedHistory.Last())
+                                                .Select(c => c.First - c.Second).ToList());
+  processedHistory.Last().Insert(0, 0);
   processedHistory.Reverse();
-  for (int i = 1; i < processedHistory.Count; i++)
-    processedHistory[i].Insert(0, processedHistory[i].First() - processedHistory[i-1].First());
+  foreach(var ph in processedHistory.Skip(1).Zip(processedHistory))
+    ph.First.Insert(0, ph.First.First() - ph.Second.First());
 
-  Console.WriteLine(processedHistory.Last().First());
-}
+  return processedHistory.Last().First();
+});
 
-Console.WriteLine(report.Sum(r => r.First()));
+Console.WriteLine(added.Sum());
