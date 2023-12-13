f8b302a6258bad57d756f7d5e9cc66191b8dd4fa
alex <alex.ioan.dragomir@gmail.com>
2023-12-13 08:29:22 +0200

    day 12 more optimizations

--- day12/day12/Program.cs
+++ day12/day12/Program.cs
@@ -1,17 +1,26 @@
-﻿var lines = File.ReadAllLines("in.txt")
+﻿using System.Diagnostics;
+
+var lines = File.ReadAllLines("in.txt")
   .Select(l => (l.Split(" ")[0], l.Split(" ")[1].Split(",").Select(t => int.Parse(t)).ToList()));
 
 
 // part1
-int sum = 0;
+long sum = 0;
+int countLine = 0;
 foreach (var line in lines)
 {
+  Stopwatch stopwatch = new Stopwatch();
+  stopwatch.Start();
+
+  countLine++;
   string row = "";
   List<int> validate = new List<int>();
-  for (int i = 0; i < 5; i++)
+
+  const int REPEAT = 5;
+  for (int i = 0; i < REPEAT; i++)
   {
     row += line.Item1;
-    if (i < 4)
+    if (i < REPEAT-1)
       row += "?";
     validate.AddRange(line.Item2);
   }
@@ -21,30 +30,31 @@ foreach (var line in lines)
 
   int total = validate.Sum();
   int countTotal = row.Count(c => c == '?' || c == '#');
+  int countHashTag = row.Count(c => c == '#');
 
   var broken = row.Select((c, i) => (c, i)).Where(c => c.c == '?').OrderBy(c => c.i).ToArray();
 
-  var generated = new HashSet<(string,int)>() { (row, countTotal) };
+  var generated = new List<(string,int, int)>() { (row, countTotal, countHashTag) };
   foreach (var b in broken)
   {
-    HashSet<(string,int)> g2 = new HashSet<(string,int)>();
+    var g2 = new List<(string,int, int)>();
     foreach (var g in generated)
     {
       string noBroken = g.Item1.Remove(b.i, 1);
       string first = noBroken.Insert(b.i, "#");
       string second = noBroken.Insert(b.i, ".");
 
-      if (IsValid(first, validate, b.i, total, g.Item2))
-        g2.Add((first, g.Item2));
+      if (IsValid(first, validate, b.i, total, g.Item2, g.Item3+1))
+        g2.Add((first, g.Item2, g.Item3 + 1));
 
-      if (IsValid(second, validate, b.i, total, g.Item2-1))
-        g2.Add((second, g.Item2 - 1));
+      if (IsValid(second, validate, b.i, total, g.Item2-1, g.Item3))
+        g2.Add((second, g.Item2 - 1, g.Item3));
     }
 
     generated = g2;
 
   }
-  var validated = generated.Where(g => IsValid(g.Item1, validate, g.Item1.Length - 1, total, int.MaxValue));
+  var validated = generated.Where(g => IsValid(g.Item1, validate, g.Item1.Length - 1, total, int.MaxValue, g.Item3)).ToList();
   bool x = false;
   if (x)
   {
@@ -52,19 +62,25 @@ foreach (var line in lines)
       Console.WriteLine(vg + "    ");
   }
 
-  Console.WriteLine(validated.Count());
+  Console.WriteLine(countLine.ToString() + ": " + validated.Count() + " - " + stopwatch.Elapsed);
   sum += validated.Count();
 }
 
 Console.WriteLine(sum);
 
-bool IsValid(string line, List<int> validation, int index, int total, int countTotal)
+bool IsValid(string line, List<int> validation, int index, int total, int countTotal, int countHashTag)
 {
   if (countTotal < total)
     return false;
 
+  if(countHashTag > total)
+    return false;
+
   string subline = line.Substring(0, index + 1);
   var tokens = subline.Split(".", StringSplitOptions.RemoveEmptyEntries);
+
+  
+
   if (index == line.Length - 1 && tokens.Length != validation.Count)
     return false;
 
@@ -74,12 +90,21 @@ bool IsValid(string line, List<int> validation, int index, int total, int countT
   if (tokens.Length > validation.Count)
     return false;
 
+  if (tokens.Any() && tokens.Last().Length < validation[tokens.Length - 1]
+      && subline.EndsWith('.'))
+    return false;
+
   for (int i = 0; i < tokens.Length - 1; i++)
     if (tokens[i].Length != validation[i])
       return false;
 
   if (index < line.Length - 1)
+  { 
+    if(tokens.Last().Length <= validation[tokens.Length - 1])
+      return true;
+
     return tokens.Last().Length <= validation[tokens.Length - 1];
+  }
   else
     return tokens.Last().Length == validation[tokens.Length - 1];
 }
