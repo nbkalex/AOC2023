fc704b26055d8a6bf98918b6f56d61051f6f8aab
alex <alex.ioan.dragomir@gmail.com>
2023-12-14 23:46:28 +0200

    day10 part2

--- day10/day10/Program.cs
+++ day10/day10/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Drawing;
-using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
 
 Dictionary<char, (Point, Point)> pipeTypes = new Dictionary<char, (Point, Point)>()
 {
@@ -8,8 +7,7 @@ Dictionary<char, (Point, Point)> pipeTypes = new Dictionary<char, (Point, Point)
   {'L', (new Point(0,-1), new Point(1, 0)) },
   {'J', (new Point(0, -1), new Point(-1, 0)) },
   {'7', (new Point(-1, 0), new Point(0,1)) },
-  {'F', (new Point(1, 0), new Point(0,1)) },
-  {'.', (new Point(10,10), new Point(10,10)) }
+  {'F', (new Point(1, 0), new Point(0,1)) }
 };
 
 var lines = File.ReadAllLines("in.txt");
@@ -26,22 +24,29 @@ for (int i = 0; i < lines.Length; i++)
 
 var startPos = pipesMap.First(pm => pm.Value == 'S').Key;
 Point next = new Point();
-foreach(var n in neighbors)
+foreach (var n in neighbors)
 {
   Point p = new Point(startPos.X + n.X, startPos.Y + n.Y);
+  if(!pipesMap.ContainsKey(p) || !pipeTypes.ContainsKey(pipesMap[p]))
+    continue;
+
   var nDir1 = pipeTypes[pipesMap[p]].Item1;
   var nDir2 = pipeTypes[pipesMap[p]].Item2;
 
   if (new Point(nDir1.X + p.X, nDir1.Y + p.Y) == startPos
    || new Point(nDir2.X + p.X, nDir2.Y + p.Y) == startPos)
   {
-    next = p; 
+    next = p;
     break;
   }
 }
 
-List<Point> loop = new List<Point>() { startPos, next };
+pipesMap[startPos] = 'L';
+
+List<Point> loop = new List<Point>() { startPos };
+next = startPos;
 var hash = loop.ToHashSet();
+var intermediates = new List<Point>();
 while (true)
 {
   var val = pipesMap[next];
@@ -50,11 +55,44 @@ while (true)
   Point next1 = new Point(next.X + dir.Item1.X, next.Y + dir.Item1.Y);
   Point next2 = new Point(next.X + dir.Item2.X, next.Y + dir.Item2.Y);
 
-  if(!hash.Contains(next1))
+  foreach (var n in neighbors)
+  {
+    Point p = new Point(next.X + n.X, next.Y + n.Y);
+    if (p != next1 && p != next2)
+    {
+      Point intermediate = new Point(next.X + n.X / 2, next.Y + n.Y / 2);
+      if (!pipesMap.ContainsKey(intermediate))
+      { 
+        pipesMap.Add(intermediate, ' ');
+        intermediates.Add(intermediate);
+      }
+    }
+    else
+    {
+      Point intermediate1 = new Point(next.X + dir.Item1.X / 2, next.Y + dir.Item1.Y / 2);
+      Point intermediate2 = new Point(next.X + dir.Item2.X / 2, next.Y + dir.Item2.Y / 2);
+
+      char val1 = intermediate1.X == next.X ? '|' : '-';
+      char val2 = intermediate2.X == next.X ? '|' : '-';
+      if (!pipesMap.ContainsKey(intermediate1))
+      { 
+        pipesMap.Add(intermediate1, val1);
+        loop.Add(intermediate1);
+      }
+      if (!pipesMap.ContainsKey(intermediate2))
+      { 
+        pipesMap.Add(intermediate2, val2);
+        loop.Add(intermediate2);
+      }
+
+    }
+  }
+
+  if (!hash.Contains(next1))
     next = next1;
   else
-  { 
-    if(!hash.Contains(next2))
+  {
+    if (!hash.Contains(next2))
       next = next2;
     else
       break;
@@ -64,20 +102,77 @@ while (true)
   hash.Add(next);
 }
 
-var notLoop = pipesMap.Keys.Where(m => !hash.Contains(m));
+//Console.WriteLine(loop.Count / 2);
 
-Console.WriteLine(loop.Count()/2);
+//const int DISPLAY_OFFSET = 10;
 
-const int DISPLAY_OFFSET = 20;
+//foreach (var m in pipesMap)
+//{
+//  Console.SetCursorPosition(DISPLAY_OFFSET + (int)(2 * m.Key.X), DISPLAY_OFFSET + (int)(2 * m.Key.Y));
+//  char display = m.Value;
 
-foreach (var m in pipesMap)
+//  if (hash.Contains(m.Key))
+//    Console.ForegroundColor = ConsoleColor.Blue;
+//  Console.WriteLine(display);
+
+//  Console.ResetColor();
+//}
+
+Console.SetCursorPosition(0, 0);
+
+Queue<Point> toVisit = new Queue<Point>();
+ 
+float minY = loop.Min(p => p.Y) + 0.5f;
+float minX = loop.Where(p => p.Y == minY).Min(p => p.X) + 0.5f;
+
+Point topLeft = new Point(minX, minY);
+toVisit.Enqueue(topLeft);
+int count = 0;
+HashSet<Point> visited = new HashSet<Point>();
+while(toVisit.Any())
 {
-  Console.SetCursorPosition(DISPLAY_OFFSET + m.Key.X, DISPLAY_OFFSET + m.Key.Y);
-  char display = m.Value;
+  Point current = toVisit.Dequeue();
 
-  if (hash.Contains(m.Key))
-    Console.ForegroundColor = ConsoleColor.Blue;
-  Console.WriteLine(display);
+  if (pipesMap.ContainsKey(current) && pipesMap[current] != ' ')
+    count++;
 
-  Console.ResetColor();
+  foreach (var n in neighbors)
+  {
+    Point f = new Point(current.X + (float)n.X/2, current.Y + (float)n.Y/2);
+    if(visited.Contains(f))
+      continue;
+
+    if(!loop.Contains(f))
+    {
+      toVisit.Enqueue(f);
+      visited.Add(f);
+    }
+  }
+}
+
+Console.Clear();
+Console.WriteLine(count);
+
+struct Point
+{
+  public override int GetHashCode()
+  {
+    return X.GetHashCode() + Y.GetHashCode();
+  }
+
+  public override bool Equals([NotNullWhen(true)] object? obj)
+  {
+    if (obj == null) return false;
+    return this == (Point)obj;
+  }
+
+  public static bool operator ==(Point p1, Point p2) => p1.X == p2.X && p1.Y == p2.Y;
+  public static bool operator !=(Point p1, Point p2) => p1.X != p2.X || p1.Y != p2.Y;
+
+  public Point(float x, float y)
+  {
+    X = x; Y = y;
+  }
+
+  public float X, Y;
 }
